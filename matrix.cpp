a99b7aa22244d04b69501453151cd7f4ee7d4cec
mikolajww <mikolaj.w.w17@gmail.com>
2017-11-26 17:05:10 +0100

    added +,+=

--- matrix.cpp
+++ matrix.cpp
@@ -23,19 +23,24 @@ void matrix::debugInfo() {
 }
 
 double matrix::read(unsigned int i, unsigned int j) const {
-	check(i,j);
+	checkRange(i,j);
 	return data->m[i][j];
 }
 
 void matrix::write(unsigned int i, unsigned int j, double val) {
-	check(i,j);
+	checkRange(i,j);
 	data = data->detach();
 	data->m[i][j] = val;
 }
 
-inline void matrix::check(unsigned int i, unsigned int j) const {
+inline void matrix::checkRange(unsigned int i, unsigned int j) const {
 	if ( (i < 0 || i > data->row) || (j < 0 || j > data->col) ) {
 		throw indexOutOfRange(i,j);
+	}
+}
+inline void matrix::checkSize(const matrix& a, const matrix& b) {
+	if(a.data->row != b.data->row || a.data->col != b.data->col) {
+		throw incompatibleSizes(a.data->row, a.data->col, b.data->row, b.data->col);
 	}
 }
 
@@ -77,10 +82,52 @@ matrix& matrix::operator= (matrix& a) {
 	return *this;
 }
 
-bool operator== (const matrix& a) const {
-	if(data->row != a.data->row || data->col != a.data->col) {
-		throw incompatibleSizes(data->row, data->col, a.data->row, a.data->col);
+bool matrix::operator== (const matrix& a) const {
+	checkSize(*this,a);
+	for (unsigned r = 0; r < data->row; ++r)	{
+		for (unsigned c = 0; c < data->col; ++c)	{
+			if(data->m[r][c] != a.data->m[r][c]) {
+				return false;
+			}
+		}
 	}
+	return true;
+}
+
+matrix operator+ (const matrix& a, const matrix& b) {
+	checkSize(a,b);
+	matrix t(a);
+	t += b;
+	return t;
+}
+
+matrix operator- (const matrix& a, const matrix& b) {
+	matrix::checkSize(a,b);
+	matrix t(a.data->row, a.data->col);
+	for (unsigned r = 0; r < a.data->row; ++r)	{
+		for (unsigned c = 0; c < a.data->col; ++c)	{
+			t.write(r,c,(a.data->m[r][c] - b.data->m[r][c]));
+		}
+	}
+	return t;
+}
+
+/*
+matrix operator* (const matrix& a, const matrix& b) {
+	if (a.data->columns != b.data->rows) {
+        throw incompatibleSizes(a.data->rows, a.data->columns, b.data->rows, b.data->columns);
+	}
+	matrix t(a.data->col, b.data->row);
 
 
+}*/
+
+matrix& matrix::operator+= (const matrix& a) {
+	matrix::checkSize(a,*this);
+	for (unsigned r = 0; r < a.data->row; ++r)	{
+		for (unsigned c = 0; c < a.data->col; ++c)	{
+			write(r,c,(a.data->m[r][c] + data->m[r][c]));
+		}
+	}
+	return *this;
 }
--- matrixtest.cpp
+++ matrixtest.cpp
@@ -11,20 +11,61 @@ int main(int argc, char const *argv[]) {
 		cout << e.what() << endl;
 	}
 	m(0,0) = 1;
-	cout << m(0,0) << endl;
-	cout << m << endl;
-	m.debugInfo();
+	//cout << m(0,0) << endl;
+	//cout << m << endl;
+	//m.debugInfo();
 	matrix m1(4,3);
-	m.debugInfo();
-	m1.debugInfo();
+	//m.debugInfo();
+	//m1.debugInfo();
 	m1(0,1) = 5;
-	m1(1,0) = 4;
-	cin >> m1;
+	
+	//cin >> m1;
 	cout << m1;
 	m = m1;
-	cout << m;
-	m.debugInfo();
-	m1.debugInfo();
-	m.debugInfo();
+	m1(1,0) = 4;
+	//cout << m;
+	//m.debugInfo();
+	//m1.debugInfo();
+	//m.debugInfo();
+	try {
+		cout << (m1 == m) << endl;
+	}
+	catch(const std::exception& e) {
+		cerr << e.what() << '\n';
+	}
+
+	try {
+		cout << "ADD" << endl;
+		cout << m1 << endl;
+		cout << m << endl;
+		cout << (m1 + m) << endl;
+	}
+	catch(const std::exception& e) {
+		cerr << e.what() << '\n';
+	}
+
+	try {
+		cout << "SUBTRACT" << endl;
+		cout << m1 << endl;
+		cout << m << endl;
+		cout << (m1 - m) << endl;
+	}
+	catch(const std::exception& e) {
+		cerr << e.what() << '\n';
+	}
+
+	try {
+		cout << "ADD +=" << endl;
+		cout << m1 << endl;
+		cout << m << endl;
+		m1 +=m;
+		cout << m1 << endl;
+	}
+	catch(const std::exception& e) {
+		cerr << e.what() << '\n';
+	}
+
+
+	
 	return 0;
 }
